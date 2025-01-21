a5387486f95b96adba2636c64b6764daec65e95f
Animesh-2211 <reachanimeshhere@gmail.com>
2025-01-21 14:46:00 +0530

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
@@ -18,6 +18,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
